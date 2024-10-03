006050d3143f6046a9a4756e6b20f1cec51ae3bc
Samuel D. Leslie <sdl@nexiom.net>
2024-10-03 21:10:11 +1000

    Slight improvements to ASP.NET Core decoder

--- src/CertUiExts/AspNetCore.c
+++ src/CertUiExts/AspNetCore.c
@@ -35,39 +35,25 @@ BOOL FormatAspNetCoreHttpsDevCert(_In_ const DWORD dwCertEncodingType,
     BOOL bStatus = FALSE;
 
     // Version
-    INT iVersion;
-    WCHAR wszVersion[4]; // Largest single byte integer
+    BYTE bVersion;
+    WCHAR* pwszPrefix;
 
     switch (cbEncoded) {
         // Empty is version zero
         case 0:
-            iVersion = 0;
+            bVersion = 0;
             break;
         // Single byte with version
         case 1:
-            iVersion = pbEncoded[0];
+            bVersion = pbEncoded[0];
             break;
         default:
             DBG_PRINT("Extension is %u bytes but expected 1 byte\n", cbEncoded);
             goto end;
     }
 
-    if (_itow_s(iVersion, wszVersion, sizeof(wszVersion) / sizeof(WCHAR), 10) != 0) {
-        DBG_PRINT("_itow_s() failed converting integer to string (errno: %d)\n", errno);
-        goto end;
-    }
-
-    if (dwFormatStrType == CRYPT_FORMAT_STR_SINGLE_LINE) {
-        if (wcscpy_s(pbFormat, *pcbFormat / sizeof(WCHAR), wszVersion) == 0) {
-            bStatus = TRUE;
-        } else {
-            DBG_PRINT("wcscpy_s() failed copying string to format buffer (errno: %d)\n", errno);
-        }
-
-        goto end;
-    }
-
-    if (swprintf_s(pbFormat, *pcbFormat / sizeof(WCHAR), L"Version %d\n", iVersion) == -1) {
+    pwszPrefix = dwFormatStrType == CRYPT_FORMAT_STR_SINGLE_LINE ? L"V" : L"Version ";
+    if (swprintf_s(pbFormat, *pcbFormat / sizeof(WCHAR), L"%s%u\n", pwszPrefix, bVersion) == -1) {
         DBG_PRINT("swprintf_s() failed formatting string to format buffer (errno: %d)\n", errno);
         goto end;
     }
